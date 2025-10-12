661842e1b7e920da8ac6f3e0d1f69646363b8a7b
Xinyu Xiang <xiangxy23@mails.tsinghua.edu.cn>
2025-10-12 09:22:23 +0800

    update: content `/` is same as `/en/`

--- astro.config.mjs
+++ astro.config.mjs
@@ -28,7 +28,7 @@ export default defineConfig({
     defaultLocale: "en",
     locales: ["en", "zh"],
     routing: {
-      prefixDefaultLocale: true,
+      prefixDefaultLocale: false,
     },
   },
 
